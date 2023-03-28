822d717b7048422eaa38e9d5cb446ac45ab54ed7
John Kinya <kingjohnkn@gmail.com>
2023-03-28 15:59:14 +0300

    update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,23 +1,27 @@
 let myEvent = [
     {
         title: "wedding",
-        date: new Date("2022-03-25"),
+        date: new Date("2023-03-25"),
         location: "Nairobi",
         attendees: new Set(["John", "Kinyua", "Kelvin", "Wambua"])
     },
     {
         title: "birthday",
-        date: new Date("2022-03-25"),
+        date: new Date("2023-03-25"),
         location: "Nairobi",
         attendees: new Set(["Daniel", "Veronicah", "Kelvin", "Anne"])
     },
     {
-        title: "part",
-        date: new Date("2022-03-25"),
+        title: "party",
+        date: new Date("2023-03-25"),
         location: "Nairobi",
         attendees: new Set(["Dan", "Yvonne", "Antony", "Wambui"])
     }
 ]
 
+// console.log(myEvent);
 // console.log(myEvent[1]);
 // console.log(myEvent.length);
+// console.log(myEvent[1].title);
+// console.log(Date());
+console.log(myEvent[2].attendees);
