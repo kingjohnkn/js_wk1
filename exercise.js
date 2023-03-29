3135eda53a55370e50882f3bb43d4c7e2f275715
John Kinya <kingjohnkn@gmail.com>
2023-03-29 15:47:58 +0300

    update excercise.js

--- exercise.js
+++ exercise.js
@@ -1,13 +1,13 @@
-let myEvent = [
+let events = [
     {
         title: "wedding",
-        date: new Date("2023-03-25"),
+        date: new Date("2023-03-30T16:00:00"),
         location: "Nairobi",
         attendees: new Set(["John", "Kinyua", "Kelvin", "Wambua"])
     },
     {
         title: "birthday",
-        date: new Date("2023-03-25"),
+        date: new Date("2023-03-29T11:00:00Z"),
         location: "Nairobi",
         attendees: new Set(["Daniel", "Veronicah", "Kelvin", "Anne"])
     },
@@ -19,9 +19,19 @@ let myEvent = [
     }
 ]
 
+const today = new Date();
+const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+const upComingEvents = events.filter((event) => {
+    return event.date >= today && event.date <= nextWeek;
+}).map((event) => {
+    return `${event.title} - ${event.date.toLocaleString()} - ${event.location}`
+})
+
+console.log(upComingEvents);
+
 // console.log(myEvent);
 // console.log(myEvent[1]);
 // console.log(myEvent.length);
 // console.log(myEvent[1].title);
 // console.log(Date());
-console.log(myEvent[2].attendees);
+// console.log(myEvent[2].attendees);
